2c34355149f48b00a763ce85b6df94543565c2b3
pareshsolanki29 <bandralites@gmail.com>
2018-12-08 19:20:39 -0500

    added

--- simon.js
+++ simon.js
@@ -26,7 +26,7 @@ console.log("strict");
     }else {
 	strict = false;
      }
-})
+});
 
 onButton.addEventListener('click', (event) => {
 	if(onButton.checked == true){
@@ -35,9 +35,168 @@ onButton.addEventListener('click', (event) => {
 	}else{
 		on = false;
 		turnCounter.innerHTML = "";
+		clearColor();
+		clearInterval(intervalId);
+	}
+});
+
+startButton.addEventListener('click', (event)=>{
+	if(on || win){
+		play();
+	}
+});
+
+function play(){
+	win =false;
+	order = [];
+	playerOrder = [];
+	flash = 0;
+	intervalId = 0;
+	turn = 1;
+	turnCounter.innerHTML = "1";
+	good = true;
+	for(var i = 0; i < 20; i++){
+		order.push(Math.floor(Math.random()* 4 )+1);
+
+	}
+	compTurn=true;
+	intervalId =setInterval(gameTurn, 800);
+
+
+};
+
+function gameTurn(){
+	on = false;
+
+	if(flash == turn){
+		clearInterval(intervalId);
+		compTurn = false;
+		clearColor();
+		on = true;
+	}
+	if(compTurn){
+		clearColor();
+		setTimeOut(() => {
+          if (order[flash] == 1) one();
+          if (order[flash] == 2) two();
+          if (order[flash] == 3) three();
+          if (order[flash] == 4) four();
+          flash++
+
+		}, 200)
+	}
+}
+
+function one(){
+	if(noise){
+		var audio = document.getElementById("clip1");
+		audio.play();
+	}
+	noise = true;
+	topLeft.style.backgroundColor = "lightgreen"
+}
+function two(){
+	if(noise){
+		var audio = document.getElementById("clip2");
+		audio.play();
+	}
+	noise = true;
+	topRight.style.backgroundColor = "tomato"
+}
+function three(){
+	if(noise){
+		var audio = document.getElementById("clip3");
+		audio.play();
+	}
+	noise = true;
+	bottomLeft.style.backgroundColor = "yellow"
+}
+function four(){
+	if(noise){
+		var audio = document.getElementById("clip4");
+		audio.play();
+	}
+	noise = true;
+	bottomRight.style.backgroundColor = "lightskyblue"
+}
+function clearColor(){
+	topLeft.style.backgroundColor = "darkgreen";
+	topRight.style.backgroundColor = "darkred";
+	bottomLeft.style.backgroundColor ="goldenrod";
+	bottomRight.style.backgroundColor ="darkblue";
+}
+
+
+topLeft.addEventListener('click', (event) =>{
+	if(on){
+		playerOrder.push(1);
+		check();
+		one();
+		if(!win){
+			setTimeOut(() =>{
+				clearColor();
+			}, 300)
+		}
 	}
 })
 
 
+topRight.addEventListener('click', (event) =>{
+	if(on){
+		playerOrder.push(2);
+		check();
+		two();
+		if(!win){
+			setTimeOut(() =>{
+				clearColor();
+			}, 300)
+		}
+	}
+})
+
+bottomLeft.addEventListener('click', (event) =>{
+	if(on){
+		playerOrder.push(3);
+		check();
+		three();
+		if(!win){
+			setTimeOut(() =>{
+				clearColor();
+			}, 300)
+		}
+	}
+})
+
+bottomRight.addEventListener('click', (event) =>{
+	if(on){
+		playerOrder.push(4);
+		check();
+		four();
+		if(!win){
+			setTimeOut(() =>{
+				clearColor();
+			}, 300)
+		}
+	}
+})
 
 
+function check(){
+	if(playerOrder[playerOrder.length -1] !== order[playerOrder.length - 1])
+		good =false;
+
+    if(playerOrder.length == 20 && good ){
+    	winGame();
+    }
+    if(good == false){
+    	flashColor();
+    	turnCounter.innerHTML = "NO";
+    	setTimeOut(() =>{
+    		turnCounter.innerHTML = turn;
+    		clearColor();    	})
+    }
+
+
+
+
+}
